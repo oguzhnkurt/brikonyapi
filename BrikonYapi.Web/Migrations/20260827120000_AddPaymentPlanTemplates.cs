ba3b6a106cd5099fcbd35bd42510f980a77ddbcf
Oguzhan Kurt <kurt4298@gmail.com>
2026-08-27 22:22:17 +0300

    Migration: eksik Npgsql using ekle (NpgsqlValueGenerationStrategy derleme hatasi CS0103)

--- BrikonYapi.Web/Migrations/20260827120000_AddPaymentPlanTemplates.cs
+++ BrikonYapi.Web/Migrations/20260827120000_AddPaymentPlanTemplates.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using BrikonYapi.Web.Data;
 
 #nullable disable
